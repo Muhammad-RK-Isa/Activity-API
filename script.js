87f2364408d8e4e27d958179a6fdfc112f3b22fe
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-02-07 22:20:25 +0600

    init

---
+++ script.js
@@ -0,0 +1,23 @@
+const newTask = () => {
+    fetch('http://www.boredapi.com/api/activity')
+    .then(response => response.json())
+    .then(data => showTask(data));
+}
+
+
+const showTask = (data) => {
+    const taskTitle = document.getElementsByClassName('task-title')[0];
+    const sourceLink = document.getElementsByClassName('source-link')[0];
+
+    taskTitle.innerText = data.activity;
+    switch (data.link !== undefined && data.link !== '') {
+        case true:
+            sourceLink.style.display = 'block';
+            sourceLink.href = data.link;
+            break;
+        case false:
+            sourceLink.style.display = 'none';
+            break;
+    };
+    console.log(data.link);
+};
